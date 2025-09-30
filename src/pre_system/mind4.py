c8873fe51d958d040adbf6a88b5883b410d1f979
ssb-uve <uve@ssb.no>
2025-09-30 13:14:34 +0200

    M

---
+++ src/pre_system/mind4.py
@@ -0,0 +1,220 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#   kernelspec:
+#     display_name: Python3
+#     language: python
+#     name: python3
+# ---
+
+# Necessary packages.
+import numpy as np
+from numpy.linalg import *
+import pandas as pd
+import warnings
+warnings.simplefilter('ignore', category=FutureWarning)
+np.set_printoptions(suppress=True)
+
+def mind4(mnr, rea, liste_d4, basisaar, startaar, freq='M'):
+    res_dict = {}
+        
+    if not (freq == 'M' or freq == 'Q'):
+        raise TypeError('The frequency setting must me either "M" or "Q".')
+    if freq == 'M':
+        freq_ = 12
+        periodely = 'monthly'
+    if freq == 'Q':
+        freq_ = 4
+        periodely = 'quarterly'
+
+    # CHECKS.
+    # Checking object type.
+    if not (type(liste_d4) == list or type(liste_d4) == str):
+        raise TypeError('You need to create a list of all the series you wish to benchmark, and it must be in the form of a list or string.')
+    
+    if type(liste_d4) == str:
+        liste_d4 = [liste_d4]
+        
+    # Checking the monthly/quarterly DF.
+    if not isinstance(mnr, pd.DataFrame):
+        raise TypeError(f'The {periodely} dataframe is not a DataFrame.') 
+    if not isinstance(mnr.index, pd.PeriodIndex):
+        raise TypeError(f'The {periodely} dataframe does not have a pd.PeriodIndex.')
+    
+    mnr_of_concern =  mnr[mnr.columns[mnr.columns.isin(liste_d4)]] # Filters out series not sent to benchmarking.
+    mnr_of_concern = mnr_of_concern[(mnr_of_concern.index.year <= basisaar) & (mnr_of_concern.index.year >= startaar)]
+    
+    if not pd.Series(liste_d4).isin(mnr.columns).all():
+        raise TypeError(f'{np.setdiff1d(liste_d4, mnr.columns).tolist()} are missing in the {periodely} dataframe.')
+    if mnr_of_concern.isna().any().any() is np.True_:
+        warnings.warn(f'There are NaN-values in {mnr_of_concern.columns[mnr_of_concern.isna().any()].to_list()} in the {periodely} dataframe.', UserWarning, stacklevel=2)
+    
+    mnrzerowarnlist = []
+    for col in mnr_of_concern.columns:
+        if (mnr_of_concern[col] == 0).all():
+            mnrzerowarnlist.append(f'{col}')
+    if len(mnrzerowarnlist) > 0:
+        warnings.warn(f'There are only zeroes in {mnrzerowarnlist} in the monthly dataframe.', UserWarning, stacklevel=2)
+
+    mnrintwarnlist = []
+    for col in mnr_of_concern.columns:
+        if not pd.api.types.is_any_real_numeric_dtype(mnr_of_concern[col]) and col not in mnr_of_concern.columns[mnr_of_concern.isna().any()].to_list():
+            mnrintwarnlist.append(f'{col}')
+    if len(mnrintwarnlist) > 0:
+        warnings.warn(f'There are values in {mnrintwarnlist} in the {periodely} dataframe that are not real numbers. Skipping sending these to benchmarking.', UserWarning, stacklevel=2)
+
+    # Checking the yearly DF.
+    if not isinstance(rea, pd.DataFrame):
+        raise TypeError('The yearly dataframe is not a DataFrame.')  
+    if not isinstance(rea.index, pd.PeriodIndex):
+        raise TypeError('The yearly dataframe does not have a pd.PeriodIndex.')
+
+    rea_of_concern =  rea[rea.columns[rea.columns.isin(liste_d4)]] # Filters out series not sent to benchmarking.
+    rea_of_concern = rea_of_concern[(rea_of_concern.index.year <= basisaar) & (rea_of_concern.index.year >= startaar)]
+    
+    if not pd.Series(liste_d4).isin(rea.columns).all():
+        raise TypeError(f'{np.setdiff1d(liste_d4, rea.columns).tolist()} are missing in the yearly dataframe.')
+    if rea_of_concern.isna().any().any() is np.True_:
+        warnings.warn(f'There are NaN-values in {rea_of_concern.columns[rea_of_concern.isna().any()].to_list()} in the yearly dataframe.', UserWarning, stacklevel=2)
+    
+    reazerowarnlist = []
+    for col in rea_of_concern.columns:
+        if (rea_of_concern[col] == 0).all():
+            reazerowarnlist.append(f'{col}')
+    if len(reazerowarnlist) > 0:
+        warnings.warn(f'There are only zeroes in {reazerowarnlist} in the yearly dataframe.', UserWarning, stacklevel=2)
+
+    reaintwarnlist = []
+    for col in rea_of_concern.columns:
+        if not pd.api.types.is_any_real_numeric_dtype(rea_of_concern[col]) and col not in rea_of_concern.columns[rea_of_concern.isna().any()].to_list():
+            reaintwarnlist.append(f'{col}')
+    if len(reaintwarnlist) > 0:
+        warnings.warn(f'There are values in {reaintwarnlist} in the yearly dataframe that are not real numbers. Skipping sending these to benchmarking.', UserWarning, stacklevel=2)
+    # reaintwarnlist
+    rea_of_concern = rea_of_concern[[col for col in rea_of_concern.columns if col not in reaintwarnlist]]
+    mnr_of_concern = mnr_of_concern[[col for col in mnr_of_concern.columns if col not in reaintwarnlist]]
+    #mnrintwarnlist
+    mnr_of_concern = mnr_of_concern[[col for col in mnr_of_concern.columns if col not in mnrintwarnlist]]
+    rea_of_concern = rea_of_concern[[col for col in rea_of_concern.columns if col not in mnrintwarnlist]]
+    liste_d4 = [serie for serie in liste_d4 if serie not in reaintwarnlist and serie not in mnrintwarnlist]
+
+
+    if not set(mnr_of_concern.index.year.unique()).difference(set(rea_of_concern.index.year)) == set():
+        raise TypeError(f"There aren't values in both series for {set(mnr_of_concern.index.year.unique()).difference(set(rea_of_concern.index.year))}.")    
+    
+    if not isinstance(startaar, int):
+        raise TypeError('The start year must be an integer.') 
+    if not isinstance(basisaar, int):
+        raise TypeError('The final year must be an integer.')
+    if not basisaar < 2050:
+        raise TypeError('The final year must be less than 2050. Are you sure you entered it correctly?.')
+    if not basisaar >= startaar:
+        raise TypeError('The start year cannot be greater than the final year.')
+
+    
+    print("The inputdata passed the checks.\n")
+    # CHECKS DONE.
+    
+    # Preperation of input data.
+    
+    # Scaling of the leading and following values.
+    avs = rea_of_concern/mnr_of_concern.resample('Y').sum()
+    
+    printlist = []
+    
+    for elem in liste_d4:
+        printlist.append(f'{elem}')
+        print(f'Benchmarking {printlist} with MinD4 from {startaar} to {basisaar}.', end="\r", flush=True)
+
+        # Laster maneds- og aarstall
+        datam_ = mnr_of_concern[elem].values
+        datay_ = rea_of_concern[elem].values
+
+   
+        # Different scaling factors for the leading and following values to ensure the volume figure is correct in the start year.
+        avstemming1 = avs[avs.index.year==startaar][elem].values
+        avstemming2 = avs[avs.index.year==basisaar][elem].values
+        
+        # The leading value.
+        if freq == 'M':
+            datamf = mnr[(mnr.index.year == startaar) & (mnr.index.month == 1)][elem].values
+        if freq == 'Q':
+            datamf = mnr[(mnr.index.year == startaar) & (mnr.index.quarter == 1)][elem].values
+
+        
+        # Scaling of the leading value.
+        datayf = avstemming1*datamf
+
+        # The following value.
+        if freq == 'M':
+            datame = mnr[(mnr.index.year == basisaar) & (mnr.index.month == freq_)][elem].values
+        if freq == 'Q':
+            datame = mnr[(mnr.index.year == startaar) & (mnr.index.quarter == freq_)][elem].values
+
+        
+        # Scaling of the following value.
+        dataye = avstemming2*datame
+        
+        datam=np.hstack((datamf,datam_,datame)) 
+        datay=np.hstack((datayf,datay_,dataye)) 
+
+        # Counting months/quarters and years.
+        nm=datam.shape[0]
+        ny=datay.shape[0]
+
+        # Setting up submatrices a for A and -vectors x for X consisting of zeros.
+        a1=np.zeros((nm,nm),dtype=np.float64)
+        a2=np.zeros((nm,ny),dtype=np.float64)
+        a3=np.zeros((ny,nm),dtype=np.float64)
+        a4=np.zeros((ny,ny),dtype=np.float64)
+        x1=np.zeros((nm,1 ),dtype=np.float64)
+        x2=np.zeros((ny,1 ),dtype=np.float64)
+
+        # Fills in submatrices a into A and -vectors x into X according to Skjæveland 1985, pages 18 and 19.
+        for i in range(0,nm):
+            a1[i,i]=(1+1*(i>0 and i<nm-1))/datam[i]**2
+            if i>0:
+                a1[i,i-1]=-1/(datam[i]*datam[i-1])
+            if i<nm-1:
+                a1[i,i+1]=-1/(datam[i]*datam[i+1])
+
+        for i in range(0,nm):
+            for j in range(0,ny):
+                if (i-1)/freq_>=j-1 and (i-1)/freq_<j:
+                    a2[i,j]=1
+                    a3[j,i]=1
+                x2[j]=datay[j]
+
+        # Combines submatrices a into A and -vectors x into X according to Skjæveland 1985, pages 18 and 19.
+        A=np.vstack((np.hstack((a1,a2)),np.hstack((a3,a4))))
+        X=np.vstack((x1,x2))
+
+        # Solves the equation system AY=X for Y if possible. Otherwise, returns null.
+        try:
+            Y=np.linalg.solve(A,X)
+        except:
+            warnings.warn(f"Wasn't able to benchmark {elem}.", UserWarning, stacklevel=2)
+            #Y=np.zeros((nm,1),dtype=np.float64) 
+
+        
+        res_dict[elem] = Y[1:nm-1].flatten()
+
+    res = pd.DataFrame(res_dict, index = mnr_of_concern.index)
+    
+    # Removing resulting NaN series from the res-DataFrame.
+    res = res[res.columns[~res.isna().any()]]
+    
+    # Checks that the deviation on the totals after benchmarking is zero for the series in liste_d4.
+    # Skips the series that already trigger error messages in the input.
+    skippe = mnr_of_concern.columns[mnr_of_concern.isna().any()].to_list() + rea_of_concern.columns[rea_of_concern.isna().any()].to_list()
+    for elem in set(liste_d4) - set(skippe):
+        if not (((res.resample('Y').sum()-rea_of_concern) >= -1) & ((res.resample('Y').sum()-rea_of_concern) <= 1)).all()[elem] is np.True_:
+            warnings.warn(f'There are deviations on the benchmarked totals in {elem} so something did not go well.', UserWarning, stacklevel=2)
+
+    print(f'\nBenchmarking with MinD4 done!')
+    
+    return res
