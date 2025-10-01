b3fdc8f817a2d0ea0cc9ad085fae06e641f8cb64
Vemund <uve@ssb.no>
2025-10-01 16:07:49 +0200

    Starte på noe

--- src/pre_system/multiplicative_benchmark.py
+++ src/pre_system/multiplicative_benchmark.py
@@ -1,4 +1,4 @@
-def benchmark(df_target: pd.DataFrame, df_indicator: pd.DataFrame):
+def multiplicative_benchmark(df_target: pd.DataFrame, df_indicator: pd.DataFrame):
     
     
     
@@ -11,7 +11,29 @@ def benchmark(df_target: pd.DataFrame, df_indicator: pd.DataFrame):
         .resample(df_indicator.index.freq)
         .ffill()
     )
-
-    return df_indicator.div(df_ratio.fillna(1).reindex(df_indicator.index))[
+    
+    res_df = df_indicator.div(df_ratio.fillna(1).reindex(df_indicator.index))[
         df_indicator.columns
     ]
+
+    return res_df
+
+
+def multiplicative_benchmark(df_target: pd.DataFrame, df_indicator: pd.DataFrame):    
+    df_ratio = (
+        df_indicator.groupby(
+            pd.PeriodIndex(df_indicator.index, freq=df_target.index.freq)
+        )
+        .sum()
+        .div(df_target)
+        .resample(df_indicator.index.freq)
+        .ffill()
+    )
+    
+    res_df = df_indicator.div(df_ratio.fillna(1).reindex(df_indicator.index))[
+        df_indicator.columns
+    ]
+
+    return res_df
+
+
